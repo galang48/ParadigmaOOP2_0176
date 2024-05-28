b41e477fdf7b95d3a18dbacf91066ad693450021
galang48 <galangyudha1945@gmail.com>
2024-05-28 11:34:15 +0700

    memanggil budi

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -31,8 +31,22 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
-int main()
-{
-    
+
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main() {
+    budi a(12);
+
+    return 0;
 }
 
