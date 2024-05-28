24b697a9dbf0bd70807a7f060383d4930bb1e966
galang48 <galangyudha1945@gmail.com>
2024-05-28 11:09:54 +0700

    menambahkan method untuk mengisi nilai

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-class AbstraksiKlas 
-{
+class AbstraksiKlas {
 private: string x, y;
 
-public :
+public:
 
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
 
 };
 int main()
