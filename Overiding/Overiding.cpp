80b280bf514bd419e3c8d588ddb2afd94aab93f3
galang48 <galangyudha1945@gmail.com>
2024-05-28 10:39:26 +0700

    menambahkan class baseClass

---
+++ Overiding/Overiding.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// class parent
+// tambahkan final sesudah nama class
+// untuk mencegah adanya overriding
+class baseClass 
+{
+
+};
+int main()
+{
+    
+}
+
+
